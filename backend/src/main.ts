5afd9aeefb5a725beaf71b76f8b1dcacae50151c
Tomás <tomicardenas96@gmail.com>
2023-09-05 19:14:03 -0300

    validation fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   //enable validation pipes.
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
   await app.listen(3080);
 }
 bootstrap();
