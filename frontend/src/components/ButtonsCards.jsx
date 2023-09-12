4d4243f3b463673e429e399a7f9e052d5a04725e
Tomás Cárdenas <tomicardenas96@gmail.com>
2023-09-11 21:38:45 -0300

    Some style adjustments

--- frontend/src/components/ButtonsCards.jsx
+++ frontend/src/components/ButtonsCards.jsx
@@ -1,33 +1,62 @@
 import { Link, NavLink } from "react-router-dom";
 import "../styles/ButtonsCards.css";
-import { useSwitchToUsers, useSwitchToClothes, useSwitchToShoes } from "./ProviderContext";
+import {
+  useSwitchToUsers,
+  useSwitchToClothes,
+  useSwitchToShoes,
+} from "./ProviderContext";
 
 function ButtonsCards() {
-    const switchToUsers = useSwitchToUsers();
-    const switchToClothes = useSwitchToClothes();
-    const switchToShoes = useSwitchToShoes();
+  const switchToUsers = useSwitchToUsers();
+  const switchToClothes = useSwitchToClothes();
+  const switchToShoes = useSwitchToShoes();
 
   return (
     <div className="buttons-container">
       <ul>
-          <li>
-            <NavLink to={"/home"}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/shoes"} onClick={switchToShoes} style={({ isActive }) => {
-        return {color : isActive ? "green" : null}
-      }}>Shoes</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/clothes"} onClick={switchToClothes} style={({ isActive }) => {
-        return {color : isActive ? "green" : null}
-      }}>Clothes</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/users"} onClick={switchToUsers} style={({ isActive }) => {
-        return {color : isActive ? "green" : null}
-      }}>Users</NavLink>
-          </li>
+        <li>
+          <NavLink
+            to={"/home"}
+            style={({ isActive }) => {
+              return { color: isActive ? "green" : null };
+            }}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/shoes"}
+            onClick={switchToShoes}
+            style={({ isActive }) => {
+              return { color: isActive ? "green" : null };
+            }}
+          >
+            Shoes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/clothes"}
+            onClick={switchToClothes}
+            style={({ isActive }) => {
+              return { color: isActive ? "green" : null };
+            }}
+          >
+            Clothes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/users"}
+            onClick={switchToUsers}
+            style={({ isActive }) => {
+              return { color: isActive ? "green" : null };
+            }}
+          >
+            Users
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
